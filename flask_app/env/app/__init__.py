4728a53012bf84dc0d01550b7285a742cbc1aa4a
joserodriguezapps <33676587+joserodriguezapps@users.noreply.github.com>
2019-09-27 23:47:37 -0700

    rest of files(python evn,ext,main)

---
+++ flask_app/env/app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import Config
+
+app = Flask(__name__)
+bootstrap =  Bootstrap(app)
+#db = SQLAlchemy(app)
+app.config.from_object(Config)
+
+from app import routes
